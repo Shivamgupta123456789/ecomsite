99d1c61e59f072b4a03f5470b9d6d760e61879b7
Shivam Gupta <guptasahab16@gmail.com>
2025-11-15 11:22:57 +0530

    first commit

---
+++ shop/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from .models import Product, Order
+from django.core.paginator import Paginator
+
+# Create your views here.
+def index(request):
+    product_objects = Product.objects.all()
+
+    #Search code
+    item_name = request.GET.get('item_name')
+    if item_name != '' and item_name is not None:
+        product_objects = product_objects.filter(title__icontains=item_name)
+
+    #paginator code
+    paginator = Paginator(product_objects,4)
+    page = request.GET.get('page')
+    product_objects = paginator.get_page(page)
+
+    return render(request, 'shop/index.html',{'product_objects':product_objects})
+
+def detail(request, id):
+    product_object = Product.objects.get(id=id)
+    return render(request, 'shop/detail.html',{'product_object':product_object})
+
+def checkout(request):
+    if request.method == "POST":
+        items = request.POST.get('items', '')
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        address = request.POST.get('address', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zipcode = request.POST.get('zip_code', '')
+        total = request.POST.get('total', '')
+
+
+        # Save the order
+        order = Order(
+            items=items,
+            name=name,
+            email=email,
+            address=address,
+            city=city,
+            state=state,
+            zipcode=zipcode,
+            total = total
+        )
+        order.save()
+
+        # Optionally show a confirmation message
+        return render(request, 'shop/checkout.html', {'success': True})
+
+    # When the method is GET, just render the empty checkout form
+    return render(request, 'shop/checkout.html')
